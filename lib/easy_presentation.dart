42edd266f45db103d7c27e5d4a49ba16f1f2b0be
Hanzla <mhanzla80@gmail.com>
2023-01-24 19:50:26 +0500

    remove body from expose.

--- lib/easy_presentation.dart
+++ lib/easy_presentation.dart
@@ -1,5 +1,4 @@
 library easy_presentation;
 
 export 'package:easy_presentation/src/easy_presentation_app.dart';
-export 'package:easy_presentation/src/easy_presentation_body.dart';
 export 'package:easy_presentation/src/models/presentation_data.dart';
